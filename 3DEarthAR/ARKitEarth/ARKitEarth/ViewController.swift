02d7543dd50d16e9045aa5c2d6d153ad037cc4dd
olizzz <xinchen.zhao.20@dartmouth.edu>
2019-09-30 01:25:40 -0400

    added plain detection and light estimation

--- 3DEarthAR/ARKitEarth/ARKitEarth/ViewController.swift
+++ 3DEarthAR/ARKitEarth/ARKitEarth/ViewController.swift
@@ -14,6 +14,12 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    var planeGeometry:SCNPlane!
+    let planeIdentifiers = [UUID]()
+    var anchors = [ARAnchor]()
+    var sceneLight: SCNLight!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -23,17 +29,33 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
+        sceneView.autoenablesDefaultLighting = false
+        
         let scene = SCNScene()
         
         // Set the scene to the view
         sceneView.scene = scene
+        
+        sceneLight = SCNLight()
+        sceneLight.type = .omni
+        
+        // create a node so that we can add to scene
+        let lightNode = SCNNode()
+        lightNode.light = sceneLight
+        lightNode.position = SCNVector3(x:0,y:10,z:2)
+        
+        sceneView.scene.rootNode.addChildNode(lightNode)
     }
     
+    // where we're actually configuring the AR session
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
+        configuration.isLightEstimationEnabled = true
+        
 
         // Run the view's session
         sceneView.session.run(configuration)
@@ -49,18 +71,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         let touch = touches.first
         let location = touch?.location(in: sceneView)
         
-        let hitResults = sceneView.hitTest(location!, types: .featurePoint)
-        
-        // check if we have a hit result
-        if let hitTestResult = hitResults.first{
-            let transform = hitTestResult.worldTransform
-            let position = SCNVector3(x: transform.columns.3.x, y:transform.columns.3.y, z:transform.columns.3.z)
-            
-            let newEarth = EarthNode()
-            newEarth.position = position
-            sceneView.scene.rootNode.addChildNode(newEarth)
-            
-        }
+        addNodeAtLocation(location: location!)
+
     }
     
     override func didReceiveMemoryWarning() {
@@ -70,14 +82,89 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     // MARK: - ARSCNViewDelegate
     
-/*
+    // this is where we can continously do updates
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        if let estimate = self.sceneView.session.currentFrame?.lightEstimate{
+            sceneLight.intensity = estimate.ambientIntensity
+        }
+    }
+
     // Override to create and configure nodes for anchors added to the view's session.
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
+        var node:SCNNode?
+        
+        //this is for the first time that we find a node for a specific anchor
+        
+        if let planeAnchor = anchor as? ARPlaneAnchor{
+            node = SCNNode()
+            planeGeometry = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
+            planeGeometry.firstMaterial?.diffuse.contents = UIColor.white.withAlphaComponent(0.5)
+            
+            let planeNode = SCNNode(geometry: planeGeometry)
+            planeNode.position = SCNVector3(x: planeAnchor.center.x, y:0,z: planeAnchor.center.z)
+            planeNode.transform = SCNMatrix4MakeRotation(-Float.pi/2, 1, 0, 0)
+            
+            updateMaterial()
+            
+            node?.addChildNode(planeNode)
+            anchors.append(planeAnchor)
+        
+            
+        }
      
         return node
     }
-*/
+    
+    // deal with updates
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        // check if we get a plane anchor
+        if let planeAnchor = anchor as? ARPlaneAnchor{
+            
+            // check if our achors array already containst this plane anchor
+            if anchors.contains(planeAnchor){
+                
+                // we already have a plane node in this area of plane anchor
+                if node.childNodes.count>0{
+                    let planeNode = node.childNodes.first!
+                    planeNode.position = SCNVector3(x:planeAnchor.center.x, y:0, z: planeAnchor.center.z)
+                    
+                    // change the geometry of our plane, update the detected surface
+                    if let plane = planeNode.geometry as? SCNPlane{
+                        plane.width = CGFloat(planeAnchor.extent.x)
+                        plane.height = CGFloat(planeAnchor.extent.z)
+                        
+                        updateMaterial()
+                        
+                    }
+                }
+            }
+        }
+    }
+    
+    func updateMaterial(){
+        let material = self.planeGeometry.materials.first!
+        
+        material.diffuse.contentsTransform = SCNMatrix4MakeScale(Float(self.planeGeometry.width), Float(self.planeGeometry.height), 1)
+    }
+    
+    func addNodeAtLocation(location:CGPoint){
+        
+        // check if we already have anchors in place that we can use
+        guard anchors.count>0 else {print("anchors are not created yet");return}
+        
+        let hitResults = sceneView.hitTest(location, types: .existingPlaneUsingExtent)
+        
+        if hitResults.count>0{
+            let result = hitResults.first!
+            let newLocation = SCNVector3(x:result.worldTransform.columns.3.x, y:result.worldTransform.columns.3.y+0.15,z:result.worldTransform.columns.3.z)
+            
+            let earthNode = EarthNode()
+            earthNode.position = newLocation
+            
+            sceneView.scene.rootNode.addChildNode(earthNode)
+        }
+    }
+
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
